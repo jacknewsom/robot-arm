7b65231e9abdd26ace05f37fbe9749357b3d7cb4
nooseman <jackmnewsom@gmail.com>
2018-03-25 21:24:28 -0700

    added contours

--- live.py
+++ live.py
@@ -1,15 +1,22 @@
 import cv2
 import numpy as np
-from skittles import find_skittles
+from skittles import *
 
 video = cv2.VideoCapture(0)
 
 while(cv2.waitKey(1) & 0xFF != ord('q')):
     ret, frame = video.read()
 
-    drawing = find_skittles(frame)
-    cv2.imshow('skittles', drawing)
-    cv2.imshow('live', frame)
+
+    param = 30
+    drawing = draw_circles(frame)
+    contours = draw_contours(frame, param)
+    binarized = binarize(np.copy(frame), param)
+
+    cv2.imshow('bin', binarized)
+    cv2.imshow('circles', drawing)
+    cv2.imshow('contours', contours)
+    # cv2.imshow('live', frame)
 
 video.release()
 cv2.destroyAllWindows()
--- skittles.py
+++ skittles.py
@@ -2,6 +2,23 @@ import cv2
 import numpy as np
 import math
 
+def binarize(image, lower_bound=30):
+    '''returns binary thresholded version of input image
+    
+    decreasing lower_bound will decrease sensitivity
+    '''
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # we're only going to work w/ saturation channel of hsv image
+    hue, saturation, value = cv2.split(hsv)
+
+    # apply gaussian blur to smooth out edges
+    gauss = cv2.GaussianBlur(saturation, (7, 7), 0)
+    # apply thresholding to eliminate noise
+    ret, threshold = cv2.threshold(gauss, lower_bound, 255, cv2.THRESH_BINARY)
+
+    return threshold
+
 def center_locations(image, radius=5):
     '''Returns list of tuples of centers of skittles '''
     locs = []
@@ -17,15 +34,7 @@ def center_locations(image, radius=5):
 
 def reduce_to_centers(image):
     '''given image of skittles, return image containing only black dots at skittle centers'''
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # we're only going to work w/ saturation channel of hsv image
-    hue, saturation, value = cv2.split(hsv)
-
-    # apply gaussian blur to smooth out edges
-    gauss = cv2.GaussianBlur(saturation, (7, 7), 0)
-    # apply thresholding to eliminate noise
-    ret, threshold = cv2.threshold(gauss, 30, 255, cv2.THRESH_BINARY)
+    threshold = binarize(image, 50)
 
     # apply distance transform, will help us find skittle centers
     dist = cv2.distanceTransform(threshold, cv2.DIST_L2, 5)
@@ -35,6 +44,7 @@ def reduce_to_centers(image):
     ret, threshold = cv2.threshold(dist, 1, 255, cv2.THRESH_TOZERO)
 
     kernel = np.ones((3,3), np.uint8)
+    kernel = kernel / (kernel.shape[0] * kernel.shape[1])
     dilation = cv2.dilate(threshold, kernel, iterations=1)
 
     ret, threshold = cv2.threshold(dilation-dist, 0, 255, cv2.THRESH_BINARY)
@@ -51,7 +61,7 @@ def reduce_to_centers(image):
     ret, centers = cv2.threshold(centers, 0, 255, cv2.THRESH_BINARY)
     return centers
 
-def find_skittles(image, radius=10):
+def draw_circles(image, radius=10):
     '''returns copy of image with circles drawn on'''
     copy = np.copy(image)
     centers = reduce_to_centers(copy)
@@ -60,3 +70,26 @@ def find_skittles(image, radius=10):
         copy = cv2.circle(copy, tuple(loc), radius, (0, 0, 255), 1)
 
     return copy
+
+def draw_contours(image, lower_bound=30):
+    '''returns copy of image with contours drawn on :) '''
+    threshold = binarize(np.copy(image), lower_bound)
+    copy = np.copy(image)
+
+    img, contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    centers, radii = [], []
+    for contour in contours:
+        epsilon = 0.1 * cv2.arcLength(contour, True)
+        poly = cv2.approxPolyDP(contour, epsilon, True)
+        center, radius = cv2.minEnclosingCircle(poly)
+        center = (int(center[0]), int(center[1]))
+        centers.append(center)
+        radii.append(radius)
+        
+        cv2.putText(copy,'R' + str(radius), center, cv2.FONT_HERSHEY_SIMPLEX, 
+                    1,(255,255,255),1,cv2.LINE_AA)
+
+
+    drawing = cv2.drawContours(copy, contours, -1, (0, 255, 0), 1)
+    return drawing
