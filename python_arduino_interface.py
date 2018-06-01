fcd01bfc575fa5fdce2f088b686dca6530e43f16
Cody Swain <swain.cody@gmail.com>
2018-05-31 20:00:15 -0700

    Added string and binary shell output

--- python_arduino_interface.py
+++ python_arduino_interface.py
@@ -12,7 +12,7 @@ class ArduinoControl():
 		arduino_ports = [
 	    p.device
 	    for p in serial.tools.list_ports.comports()
-	    if 'Generic' in p.description
+	    if 'Generic' in p.description 	#Change to REGEX (Generic|Arduino)
 		]
 		if not arduino_ports:
 		    raise IOError("No Arduino found")
@@ -24,11 +24,14 @@ class ArduinoControl():
 
 	#motor num = 1-4, motor_dir 1 or 0, motor_angle 000-360
 	def move_motor(self, motor_num=0, motor_dir=0, motor_angle=0):
-		motor_instructions = motor_num + motor_dir + motor_angle
+		motor_instructions = str(motor_num) + str(motor_dir) + str(motor_angle)
+		print("Motor instructions: " + str(motor_instructions))
+		print("Serial binary input: " + str(motor_instructions.encode()))
 		self.ser.write(motor_instructions.encode())
 
 
 if __name__ == "__main__":
 	ac = ArduinoControl()
-	ac.connect()A
+	ac.connect()
+	ac.move_motor(1,1,1)
 
